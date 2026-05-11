9034fa76c2f786522707ca67add468ed7325fa22
Zoe <zoeinspired1609@gmail.com>
2026-05-11 11:56:38 +0100

    DuplicatedElimination.java

---
+++ DuplicatedElimination.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DuplicatedElimination{
+
+public static void main(String[] args){
+
+Scanner input = new Scanner(System.in);
+
+int[] numbers = new int[101];
+
+
+System.out.println("Enter a number");
+int userInput = input.nextInt();
+
+//int[] allValues = -1;
+
+    int count = 0; 
+
+    for(count = 10; count < numbers.length; count++){
+    
+    System.out.println()
+    
+    }
+
+ 
+    
+        
+
+
+
+}
+
+
+}
