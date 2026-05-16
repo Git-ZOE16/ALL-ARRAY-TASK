3cd0d5869455321ac21a67c37bab0fa0a6cacc88
Zoe <zoeinspired1609@gmail.com>
2026-05-16 22:31:14 +0100

    All-Array-Task and Fire Drill Folder

---
+++ Fire Drill/TaskFive.java
@@ -0,0 +1,23 @@
+public class TaskFive{
+    public static void main(String[] args){
+    
+ int[] duplicateValues = {1,5,5,5,6,8,9};
+ 
+ 
+ System.out.println(numberOccurence(duplicateValues));
+ 
+    }
+public static int numberOccurence(int[] duplicateValues){
+
+int count = 0;
+    
+    for (count = 0; count < duplicateValues.length; count++){
+    
+    
+    } 
+   return count;
+
+}
+    
+}
+   
---
+++ Fire Drill/TaskSix.java
@@ -0,0 +1,26 @@
+public class TaskSix{
+public static void main(String[] args){
+
+int[] array = new array[0];
+
+int number = 0;
+
+int count = 0; 
+
+for(int count = 0; count < array.lenght; count--){
+
+    //for(number = count > array.lenght; number);
+    
+    return count;
+    
+
+}
+
+
+
+
+}
+
+
+
+}
